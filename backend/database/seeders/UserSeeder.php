0697254ae04ce9b91000f0278a19b1b0d3fe65f3
lima-bryan <99940872+limabryan@users.noreply.github.com>
2025-08-16 03:46:50 -0300

    feat: atualizei as count nas seeds e fiz o teste da routte (teste ok)

--- backend/database/seeders/UserSeeder.php
+++ backend/database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 
 class UserSeeder extends Seeder
@@ -16,6 +17,26 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        User::factory()->count(10)->create();
+          User::factory()->count(20)->create();
+
+        //usuario para teste
+        User::create([
+            'company_id' => 1,
+            'first_name' => 'Bryan',
+            'last_name' => 'Lima',
+            'email' => 'admin@email.com',
+            'phone' => '11999999999',
+            'password' => Hash::make('12345678'), //senha criptografada
+            'email_verified_at' => now(), //pra vereficar a data de verificação do email
+        ]);
+        User::create([
+            'company_id' => 1,
+            'first_name' => 'Marcos',
+            'last_name' => 'Lima',
+            'email' => 'admin2@email.com',
+            'phone' => '11999999999',
+            'password' => Hash::make('12345678'), //senha criptografada
+            'email_verified_at' => now(), //pra vereficar a data de verificação do email
+        ]);
     }
 }
