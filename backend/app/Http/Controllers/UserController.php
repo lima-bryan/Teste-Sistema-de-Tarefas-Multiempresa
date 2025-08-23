bb6d188081e873601c8109bb221e16fbf3109eb5
lima-bryan <99940872+limabryan@users.noreply.github.com>
2025-08-23 16:31:03 -0300

    feat: implementar autenticação JWT para gerenciamento de tarefas

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
+use Tymon\JWTAuth\Facades\JWTAuth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rule;
 use App\Models\User;
@@ -18,8 +18,9 @@ class UserController extends Controller
      */
     public function index()
     {
-        return User::where('company_id', Auth::user()->company_id)->paginate(5); //retorna todos os usuarios da empresa do usuario autenticado, pagina 5 por vez, 
-        // ->get();         para retornar todos os usuarios
+        $user = JWTAuth::parseToken()->authenticate();
+        return User::where('company_id', $user->company_id)->paginate(5); //retorna todos os usuarios da empresa do usuario autenticado, pagina 5 por vez,
+        // ->get();         para retornar todos os usuarios
     }
 
     /**
@@ -31,6 +32,8 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
+        $auth_user = JWTAuth::parseToken()->authenticate();
+
         $validated = $request->validate([
             //se nao colocar | da erro no banco pq o laravel vai reconhecer como uma unica regra
             'first_name' => 'required|string|max:50',
@@ -39,12 +42,11 @@ class UserController extends Controller
             'email' => 'required|email|unique:users', //valida o email se for unico
             'password' => 'required|string|min:8|confirmed' //valida a senha se for maior que 8 caracteres
         ]);
-        
-        $validated['password'] = Hash::make($validated['password']); //criptografando a senha
-        $validated['company_id'] = Auth::user()->company_id; //associa o usuario a empresa do usuario autenticado
-        $user = User::create($validated);
-        return response()->json($user, 201); 
 
+        $validated['password'] = Hash::make($validated['password']); //criptografa a senha
+        $validated['company_id'] = $auth_user->company_id; //associa o usuario a empresa do usuario autenticado
+        $user = User::create($validated);
+        return response()->json($user, 201);
     }
 
     /**
@@ -52,17 +54,19 @@ class UserController extends Controller
      *
      * @param  int  $id
      * @return \Illuminate\Http\Response
-     * endpoint get 
-     * 
+     * endpoint get
+     *
      */
     public function show(User $user)
     {
-        if ($user->company_id !== Auth::user()->company_id) {
-            return response()->json(['AVISO' => 'Usuário não encontrado.'], 404);
+        $auth_user = JWTAuth::parseToken()->authenticate();
+
+        if ($user->company_id !== $auth_user->company_id) {
+            return response()->json(['AVISO' => 'O usuário não foi encontrado.'], 404);
         } else{
-            return response()->json($user); 
-        }  
-    } 
+            return response()->json($user);
+        }
+    }
 
     /**
      * Update the specified resource in storage.
@@ -72,22 +76,27 @@ class UserController extends Controller
      * @return \Illuminate\Http\Response
      * endpoint:put/patch *
      */
-    public function update(Request $request, $user)
+    public function update(Request $request, User $user)
     {
-        if ($user->company_id !== Auth::user()->company_id) {
+        $auth_user = JWTAuth::parseToken()->authenticate();
+
+        if ($user->company_id !== $auth_user->company_id) {
             return response()->json(['AVISO' => 'Usuário não encontrado!'], 404);
         }
-         //valida os dados do usuario
+        //valida os dados do usuario
         $validated = $request->validate([
-            'fitst_name' => 'required|string|max:50',
-            'last_name' => 'required|string|max:50',
+            'first_name' => 'sometimes|string|max:50',
+            'last_name' => 'sometimes|string|max:50',
+            'phone' => 'sometimes|string|max:15',
             'email' => ['sometimes', 'email', Rule::unique('users')->ignore($user->id)],
-            'password' => 'nullable string min:8 confirmed', ]); 
+            'password' => 'nullable|string|min:8|confirmed'
+        ]);
 
-        if ($request->filled('password'))  //aqui eu verifico se a senha foi preenchida
+        if ($request->filled('password'))  //verifica se a senha foi preenchida
         {
-            $user->password = Hash::make($validated['password']);
+            $validated['password'] = Hash::make($validated['password']);
         }
+
         $user->update($validated);
         return response()->json(['AVISO' => 'Usuario atualizado com sucesso!'], 200);
     }
@@ -100,7 +109,9 @@ class UserController extends Controller
      */
     public function destroy(User $user)
     {
-        if ($user->company_id !== Auth::user()->company_id) {
+        $auth_user = JWTAuth::parseToken()->authenticate();
+
+        if ($user->company_id !== $auth_user->company_id) {
             return response()->json(['AVISO' => 'Usuário não encontrado!'], 404);
         }
         $user->delete();
